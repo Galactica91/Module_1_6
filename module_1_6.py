4899a1d254b25fa2a0cf923f68d6294e4c2381e8
Galactica91 <osheva91@yandex.com>
2024-07-23 16:07:27 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+my_dict = {'Tania': 2002, 'Olga': 1991, 'Alex': 1965}
+print(my_dict)
+print(my_dict['Tania'])
+print(my_dict.get('Denis'))
+my_dict['Denis'] = 1955
+my_dict['Rose'] = 1984
+print(my_dict)
+my_dict.pop('Denis')
+print(my_dict)
